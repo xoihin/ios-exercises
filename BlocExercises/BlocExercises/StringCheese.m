4226ce2f8250036bf22f4421200f0e3bb9e395d5
Xoi Hin <xoihin@yahoo.com>
2015-08-12 16:39:00 -0400

    Assignment 09 - Strings completed

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -12,35 +12,60 @@
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
     /* WORK HERE */
-    return nil;
+//    return nil;
+    return [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
+    
+    NSString *resultString = [[NSString alloc]init];
+    
     if ([[cheeseName lowercaseString] hasSuffix:@" cheese"]) {
         /* WORK HERE, ASSUMING `cheeseName` ENDS WITH " cheese" */
+        NSRange searchRange = [cheeseName rangeOfString:@"cheese"];
+        resultString = [cheeseName substringToIndex:[cheeseName length] - (searchRange.length + 1)];
+        
     } else {
         /* WORK HERE, ASSUMING `cheeseName` DOES NOT END WITH " cheese" */
+        resultString = [NSString stringWithString:cheeseName];
     }
 
     /*
      (You will learn more about if/else statements in a later checkpoint.)
      */
-
-    return nil;
+    
+    if ([[cheeseName capitalizedString] hasSuffix:@" Cheese"]) {
+        NSRange searchRange = [cheeseName rangeOfString:@"cheese" options:NSCaseInsensitiveSearch];
+        resultString = [cheeseName substringToIndex:[cheeseName length] - (searchRange.length + 1)];
+    }
+    
+    
+//    return nil;
+    return resultString;
+    
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+    
+    NSString *totalCheese = [[NSString alloc]init];
+    
     if (cheeseCount == 1) {
         /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        totalCheese = [NSString stringWithFormat:@"%ld cheese", cheeseCount];
+        
     } else {
         /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        totalCheese = [NSString stringWithFormat:@"%ld cheeses", cheeseCount];
     }
     
     /*
      (You will learn more about if/else statements in a later checkpoint.)
      */
     
-    return nil;
+//    return nil;
+    return totalCheese;
+    
 }
 
 @end
