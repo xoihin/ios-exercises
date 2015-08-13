5921fb2c3d229f961ec21d19f5b998e34e109115
Xoi Hin <xoihin@yahoo.com>
2015-08-13 15:29:24 -0400

    Checkpoint 12 completed, loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,32 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSString *resultString = [[NSString alloc]init];
+    
+    // First number is less than second number.
+    if (number < otherNumber) {
+        
+        for (NSInteger n = number ; n <= otherNumber; n++) {
+            NSString* myNewString = [NSString stringWithFormat:@"%ld", (long)n];
+            // NSLog(@"%@", myNewString);
+            resultString = [NSString stringWithFormat:@"%@%@", resultString, myNewString];
+        }
+        
+        // First number is greater than second number.
+    } else if (number > otherNumber) {
+        
+        for (NSInteger n = otherNumber ; n <= number; n++) {
+            NSString* myNewString = [NSString stringWithFormat:@"%ld", (long)n];
+            resultString = [NSString stringWithFormat:@"%@%@", resultString, myNewString];
+        }
+        
+        // First number is equal to second number.
+    } else if (number == otherNumber) {
+        
+        resultString = [NSString stringWithFormat:@"%ld", (long)number];
+    }
+    
+    return resultString;
 }
 
 @end
