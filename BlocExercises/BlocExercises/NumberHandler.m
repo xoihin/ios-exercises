f0f896554bf55fc2dc3aa0105da2d5e696c7ed8f
Xoi Hin <xoihin@yahoo.com>
2015-08-15 01:56:11 -0400

    Assignment 15 completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,38 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    
+    return @([number floatValue] * 2);
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *myReturnArray = [[NSMutableArray alloc]init];
+    
+    for (NSInteger n = number; n <= otherNumber; n++) {
+        [myReturnArray addObject:[NSNumber numberWithInteger:n]];
+    }
+    
+    return myReturnArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    
+    // Create an mutable array so it can be sorted
+    NSMutableArray *myReturnArray = [NSMutableArray arrayWithArray:arrayOfNumbers];
+    
+    // Define sort criteria
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:NO];
+    
+    // Sort array
+    [myReturnArray sortUsingDescriptors:@[sortDescriptor]];
+    
+    // Get the last element in the array which contains the lowest number.
+    NSInteger lowestNumber = [[myReturnArray lastObject]integerValue];
+    
+    return lowestNumber;
 }
 
 @end
